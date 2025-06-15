7cc4c58c517adf81f6f0e6b3db0c70f0ac659aea
Devin Xie <devinxie418@gmail.com>
2025-06-14 23:25:19 -0400

    added parallax & tilt to pokemon cards

---
+++ public/modules/parallax.js
@@ -0,0 +1,16 @@
+const parallaxBg = () => {
+    const homeBackground = document.querySelector(".parallax-bg");
+    const allPokemonView = document.querySelector(".all-pokemon-view");
+
+    if (!homeBackground) return;
+
+    window.addEventListener("mousemove", function(e) {
+        const xPos = (e.clientX / window.innerWidth - 0.5) * 2;
+        const yPos = (e.clientY / window.innerHeight - 0.5) * 2;
+
+        homeBackground.style.transform = `translate(${xPos * 20}px, ${yPos * 20}px)`;
+        allPokemonView.style.transform = `translate(${xPos * 5}px, ${yPos * 5}px)`;
+    });
+};
+
+parallaxBg();
