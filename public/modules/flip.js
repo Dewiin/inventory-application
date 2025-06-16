8e56ee9b43b4fcdaa06900bbd35dcde0065a95d7
Devin Xie <devinxie418@gmail.com>
2025-06-16 13:11:33 -0400

    remove user select, will implement trainers

--- public/modules/flip.js
+++ public/modules/flip.js
@@ -4,25 +4,31 @@ cards.forEach((card) => {
 	card.addEventListener("click", async () => {
 		const name = card.dataset.name;
 
-        const frontCard = document.querySelector(`.pokemon-view.card-front[data-name="${name}"]`);
-        const backCard = document.querySelector(`.pokemon-view.card-back[data-name="${name}"]`);
+		const frontCard = document.querySelector(
+			`.pokemon-view.card-front[data-name="${name}"]`,
+		);
+		const backCard = document.querySelector(
+			`.pokemon-view.card-back[data-name="${name}"]`,
+		);
 
 		const response = await fetch(`/api/pokemon/${name}/details`);
 		const data = await response.json();
 
-		const typesElement = document.querySelector(`.pokemon-view.card-back[data-name="${name}"] .types`);
+		const typesElement = document.querySelector(
+			`.pokemon-view.card-back[data-name="${name}"] .types`,
+		);
 		typesElement.replaceChildren();
 
 		data.types.forEach((type) => {
 			const typeImg = document.createElement("img");
 			typeImg.src = type.sprite;
 			typeImg.alt = type.name;
-			typeImg.classList.add("type-view"); 
+			typeImg.classList.add("type-view");
 			typesElement.appendChild(typeImg);
 		});
 
-        card.classList.toggle("active");
-        frontCard.classList.toggle("active");
-        backCard.classList.toggle("active");
+		card.classList.toggle("active");
+		frontCard.classList.toggle("active");
+		backCard.classList.toggle("active");
 	});
 });
--- public/modules/parallax.js
+++ public/modules/parallax.js
@@ -6,8 +6,6 @@ const parallaxBg = () => {
 	const icons = document.querySelector(".icons");
 	const typesNavbar = document.querySelector(".types-navbar");
 
-	if (!homeBackground) return;
-
 	window.addEventListener("mousemove", function (e) {
 		const xPos = (e.clientX / window.innerWidth - 0.5) * 2;
 		const yPos = (e.clientY / window.innerHeight - 0.5) * 2;
@@ -24,7 +22,9 @@ const parallaxBg = () => {
 		const yPos = (e.clientY / window.innerHeight - 0.5) * 2;
 
 		icons.style.transform = `translate(${xPos * 10}px, ${yPos * 10}px)`;
-		typesNavbar.style.transform = `translate(${xPos * 10}px, ${yPos * 10}px)`;
+		if (typesNavbar) {
+			typesNavbar.style.transform = `translate(${xPos * 10}px, ${yPos * 10}px)`;
+		}
 	});
 };
 
